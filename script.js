1cb713f76f420c222e2c462fbdaa9168e7a93c52
mrosya <88544601+mrosya@users.noreply.github.com>
2022-12-20 23:19:49 +0300

    Update script.js

--- script.js
+++ script.js
@@ -21,3 +21,21 @@ function draw() {
         ctx.fill();
     }
 }
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+
+        let dropdowns = document.getElementsByClassName("dropcontent");
+        let i;
+        for(i = 0; i < dropdowns.length; i++) {
+            let openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }   
+    }
+}
